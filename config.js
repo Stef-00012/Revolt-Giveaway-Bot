33835ae5e5054dac21fa249113df4892ddf721dd
Stef <stefano.delprete.08@gmail.com>
2022-03-09 09:23:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const config = {
   bot: {
-    token: '4YDVYUW2ZxZvjEkER4ip-mzagFFCtGsQgm63zKhxx6Xp6F0u_93g6stV6HXfl4NL',
+    token: '<TOKEN>',
     prefix: '.'
   },
   mongo: {
-    uri: 'mongodb+srv://Stefano:3odeR8BePrY4LAbs@discordbots.lrxkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    uri: '<MONGO_URI>'
   }
 }
